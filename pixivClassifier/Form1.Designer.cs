57bb0eafc3fce91fc30129a5a3d5ef708e603847
Einsbon Kim <einsbon@gmail.com>
2017-10-04 21:16:55 +0900

    초기 커밋.

---
+++ pixivClassifier/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace pixivClassifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClassify = new System.Windows.Forms.Button();
+            this.btnClassifyStop = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.btnFolderPaste = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtTag = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClassify
+            // 
+            this.btnClassify.Location = new System.Drawing.Point(886, 81);
+            this.btnClassify.Name = "btnClassify";
+            this.btnClassify.Size = new System.Drawing.Size(83, 27);
+            this.btnClassify.TabIndex = 1;
+            this.btnClassify.Text = "분류 시작";
+            this.btnClassify.UseVisualStyleBackColor = true;
+            this.btnClassify.Click += new System.EventHandler(this.btnClassify_Click);
+            // 
+            // btnClassifyStop
+            // 
+            this.btnClassifyStop.Location = new System.Drawing.Point(975, 81);
+            this.btnClassifyStop.Name = "btnClassifyStop";
+            this.btnClassifyStop.Size = new System.Drawing.Size(83, 27);
+            this.btnClassifyStop.TabIndex = 2;
+            this.btnClassifyStop.Text = "분류 정지";
+            this.btnClassifyStop.UseVisualStyleBackColor = true;
+            this.btnClassifyStop.Click += new System.EventHandler(this.btnClassifyStop_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "복사시키기";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 34);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "이동시키기";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(12, 62);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(135, 23);
+            this.btnFolder.TabIndex = 5;
+            this.btnFolder.Text = "분류 대상 폴더 설정";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // btnFolderPaste
+            // 
+            this.btnFolderPaste.Location = new System.Drawing.Point(12, 91);
+            this.btnFolderPaste.Name = "btnFolderPaste";
+            this.btnFolderPaste.Size = new System.Drawing.Size(135, 23);
+            this.btnFolderPaste.TabIndex = 6;
+            this.btnFolderPaste.Text = "복사/이동할 폴더 설정";
+            this.btnFolderPaste.UseVisualStyleBackColor = true;
+            this.btnFolderPaste.Click += new System.EventHandler(this.btnFolderPaste_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 130);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1069, 519);
+            this.webBrowser1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtTag
+            // 
+            this.txtTag.Location = new System.Drawing.Point(372, 11);
+            this.txtTag.Multiline = true;
+            this.txtTag.Name = "txtTag";
+            this.txtTag.Size = new System.Drawing.Size(334, 109);
+            this.txtTag.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Location = new System.Drawing.Point(229, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(125, 40);
+            this.panel1.TabIndex = 12;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(0, 0);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 16);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "하나 이상을 포함";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(0, 22);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(87, 16);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "전체를 포함";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "(설정 안됨)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "(설정 안됨)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "태그 옵션:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(741, 28);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(228, 22);
+            this.progressBar1.TabIndex = 16;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(975, 34);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(61, 12);
+            this.lblProgress.TabIndex = 17;
+            this.lblProgress.Text = "진행 상황:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(741, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 16);
+            this.checkBox1.TabIndex = 18;
+            this.checkBox1.Text = "브라우저 보이기";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 661);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtTag);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnFolderPaste);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnClassifyStop);
+            this.Controls.Add(this.btnClassify);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClassify;
+        private System.Windows.Forms.Button btnClassifyStop;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Button btnFolderPaste;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtTag;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ pixivClassifier/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace pixivClassifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string imgPath;
+        string pastePath;
+        int count = 0;
+        bool working = false;
+
+        private void btnClassify_Click(object sender, EventArgs e)
+        {
+            StringReader strReader = new StringReader(txtTag.Text);
+            List<string> userTags = new List<string>();
+            string line;
+            while (true)
+            {
+                line = strReader.ReadLine();
+                if(line != null)
+                {
+                    userTags.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            strReader.Dispose();
+
+            if (Directory.Exists(imgPath)& Directory.Exists(pastePath))
+            {
+                working = true;
+                progressBar1.Enabled = true;
+                DirectoryInfo di = new DirectoryInfo(imgPath);
+                int fileCount = di.GetFiles().Length;
+                progressBar1.Step = fileCount;
+                int workCount = 0;
+                foreach(var img in di.GetFiles())
+                {
+                    string imgNumStr = img.Name.Substring(0, img.Name.IndexOf("_"));
+                    webBrowser1.Navigate("https://www.pixiv.net/member_illust.php?mode=medium&illust_id="+imgNumStr);
+                    while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                    {
+                        Application.DoEvents();
+                    }
+                    
+                    HtmlDocument hd = webBrowser1.Document;
+                    HtmlElementCollection hecTags = hd.GetElementsByTagName("li");
+                    List<string> tagList = new List<string>();
+                    foreach (HtmlElement he in hecTags)
+                    {
+
+                        if (he.GetAttribute("className").ToString() == "tag")
+                        {
+                            tagList.Add(he.Children[1].InnerHtml);
+                        }
+                    }
+
+                    bool move = false;
+                    if(radioButton3.Checked == true) //하나 이상을 포함
+                    {
+                        foreach(string item in userTags)
+                        {
+                            if (tagList.Contains(item))
+                            {
+                                move = true;
+                            }
+                        }
+                    }
+                    else //전체를 포함
+                    {
+                        move = true;
+                        foreach(string item in userTags)
+                        {
+                            if (!tagList.Contains(item))
+                            {
+                                move = false;
+                            }
+                        }
+                    }
+
+                    if(move == true)
+                    {
+                        if (radioButton1.Checked == false)
+                        {
+                            img.MoveTo(pastePath + "\\" + Path.GetFileName(img.FullName));
+                        }
+                        else
+                        {
+                            img.CopyTo(pastePath + "\\" + Path.GetFileName(img.FullName), true);
+                        }
+                    }
+                    tagList.Clear();
+                    progressBar1.PerformStep();
+                    workCount++;
+                    lblProgress.Text = "진행 상황: " + workCount + "/" + fileCount;
+
+                    if(working == false)
+                    {
+                        break;
+                    }
+                }
+
+                if (working == true)
+                {
+                    webBrowser1.DocumentText = "";
+                    // webBrowser1.Navigate("about:blank");
+                    userTags.Clear();
+                    lblProgress.Text = "진행 상황: 완료됨";
+                    progressBar1.Enabled = false;
+                    MessageBox.Show("분류 완료");
+                }
+
+                
+            }
+            else
+            {
+                MessageBox.Show("대상 폴더, 복사/이동할 폴더의 경로를 설정하시오!");
+            }
+            
+        }
+
+        private void btnClassifyStop_Click(object sender, EventArgs e)
+        {
+            working = false;
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            if(this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imgPath = this.folderBrowserDialog1.SelectedPath;
+                label1.Text = imgPath;
+            }
+        }
+
+        private void btnFolderPaste_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pastePath = this.folderBrowserDialog1.SelectedPath;
+                label2.Text = pastePath;
+            }
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.pixiv.net/");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == false)
+            {
+                webBrowser1.Visible = false;
+            }
+            else
+            {
+                webBrowser1.Visible = true;
+            }
+        }
+    }
+}
